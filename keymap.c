f84d2c8e821dfc0c3933862a4649e7ec0ac8ce1d
shiro01 <8133535+shiro01@users.noreply.github.com>
2019-02-15 17:43:56 +0900

    fix

--- keymap.c
+++ keymap.c
@@ -32,9 +32,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______, _______,    _______,   _______, _______, _______, _______, _______, _______
     ),
     [_LR2] = LAYOUT(
-        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,       KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-        _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______,       XXXXXXX, KC_WH_U, KC_MS_L, KC_MS_U, KC_MS_R, KC_BTN1, KC_BTN3,
-        _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    S(KC_CAPS),    XXXXXXX, KC_WH_D, KC_WH_L, KC_MS_D, KC_WH_R, KC_BTN2, XXXXXXX,
-        _______, _______, _______, _______, _______, _______, _______,       _______, _______, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX
+        _______, KC_F1,      KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,    KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+        _______, KC_1,       KC_2,    KC_3,    KC_4,    KC_5,    _______,    XXXXXXX, KC_WH_U, KC_MS_L, KC_MS_U, KC_MS_R, KC_BTN1, KC_BTN3,
+        _______, KC_6,       KC_7,    KC_8,    KC_9,    KC_0,    KC_END,     XXXXXXX, KC_WH_D, KC_WH_L, KC_MS_D, KC_WH_R, KC_BTN2, XXXXXXX,
+        _______, S(KC_CAPS), _______, _______, _______, _______, _______,    _______, _______, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX
     )
 };
