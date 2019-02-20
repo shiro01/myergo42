0b0899b7a5c039213cd38750aa5a2af95d4039ab
shiro01 <8133535+shiro01@users.noreply.github.com>
2019-02-20 10:53:38 +0900

    fix

--- keymap.c
+++ keymap.c
@@ -20,10 +20,10 @@ extern keymap_config_t keymap_config;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT(
-        KC_ESC,  KC_Q,         KC_W,    KC_E,           KC_R,              KC_T,              KC_PSCR,    KC_INS,  KC_Y,     KC_U,     KC_I,    KC_O,   KC_P,            KC_BSPC,
-        KC_TAB,  KC_A,         KC_S,    KC_D,           KC_F,              KC_G,              KC_DELT,    JP_LBRC, KC_H,     KC_J,     KC_K,    KC_L,   RSFT_T(KC_SCLN), JP_COLN,
-        KC_LSFT, KC_Z,         KC_X,    KC_C,           KC_V,              KC_B,              KC_HOME,    JP_RBRC, KC_N,     KC_M,     KC_COMM, KC_DOT, RCTL_T(KC_SLSH), KC_MINS,
-        KC_LCTL, LCTL(KC_TAB), KC_LWIN, LALT_T(KC_ENT), LT(_LR1, KC_MHEN), LT(_LR2, KC_ZKHK), KC_SPC,     KC_ENT,  MO(_LR1), MO(_LR2), JP_HAT,  JP_AT,  JP_BSLS,         JP_YEN
+        KC_ESC,  KC_Q,         KC_W,    KC_E,           KC_R,              KC_T,   KC_PSCR,               KC_INS,  KC_Y,     KC_U,     KC_I,    KC_O,   KC_P,            KC_BSPC,
+        KC_TAB,  KC_A,         KC_S,    KC_D,           KC_F,              KC_G,   KC_DELT,               JP_LBRC, KC_H,     KC_J,     KC_K,    KC_L,   RSFT_T(KC_SCLN), JP_COLN,
+        KC_LSFT, KC_Z,         KC_X,    KC_C,           KC_V,              KC_B,   KC_HOME,               JP_RBRC, KC_N,     KC_M,     KC_COMM, KC_DOT, RCTL_T(KC_SLSH), KC_MINS,
+        KC_LCTL, LCTL(KC_TAB), KC_LWIN, LALT_T(KC_ENT), LT(_LR1, KC_MHEN), KC_SPC, LT(_LR2, KC_ZKHK),     KC_ENT,  MO(_LR1), MO(_LR2), JP_HAT,  JP_AT,  JP_BSLS,         JP_YEN
     ),
     [_LR1] = LAYOUT(
         _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______,    KC_PAUSE,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
